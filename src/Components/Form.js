5e292d2134c37f297f8558309e987b2422df540d
LunarExecutor <laghauna@gmail.com>
2024-06-07 23:09:30 -0700

    add comments

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -23,14 +23,16 @@ export default function Form({ OnAddItems }) {
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => ( // i wrote this to get the numbers from 1 to 20 in an array then i used map to display them
+
+          <option value={num} key={num}> 
+            {num} 
+          </option> // num is the value of the array
         ))}
       </select>
       <input type="text" placeholder="Item..." value={description}
-        onChange={(e) => setDescription(e.target.value)} />
+      // controlled elements for input
+        onChange={(e) => setDescription(e.target.value)} /> 
       <button>Add</button>
     </form>
   );
